d9543e1dd052c31693afe04e0d8898459b2cb248
iamblue <tonyone0902@gmail.com>
2013-11-03 05:39:32 +0800

    update add post api

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,6 +4,7 @@
 module.exports = function ($youmeb,$sequelize) {
 
   var Posts = $sequelize.model('Posts');
+  var Tags = $sequelize.model('Tags');
 
   this.$({
     name: 'posts',
@@ -28,27 +29,40 @@ module.exports = function ($youmeb,$sequelize) {
       res.send('login');
     }
   };
-  
   this.updatepost = {
     path: '/update',
     methods: ['post'],
     handler: function (req, res, next) {
-      
+      var data = req.body.data;//console.log(req.body.data);
+      var picid = req.body.picid;
+      var tmp = [];
+      for (var i=0;i<data.length;i++){ 
+        tmp.push({postid:1,tagid:data[i].id,tagname:data[i].name,x:data[i].x,y:data[i].y,sitename:'7-11南港展覽館分店',addr:'南港展覽館',groupname:'衣櫃'});
+      };
       Posts.create({
         userid:'1',
         status: 0,
+        content:req.body.data,
         x:0,
         y:0,
         sitename:'7-11南港分店',
         addr:'南港展覽館',
         notice:'none',
         point:0
-      }).success(function(final){
+      }).success(function (final, err){
         console.log(final);
-        res.send({
-          res:true,
-          postid:1
-        })
+        console.log(err);
+        // Tags.bulkCreate(tmp).success(function(d,e) { // Notice: There are no arguments here, as of right now you'll have to...
+        //   console.log(d);
+        //   console.log(e);
+        //   res.send({
+        //     res:true
+        //   })
+        // })
+        // res.send({
+        //   res:true,
+        //   postid:1
+        // })
       })
     }
   };
