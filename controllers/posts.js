922d97a84139ab021eba2069f1569b68d144e5cf
iamblue <tonyone0902@gmail.com>
2013-11-03 03:46:49 +0800

    update

--- controllers/posts.js
+++ controllers/posts.js
@@ -30,10 +30,26 @@ module.exports = function ($youmeb,$sequelize) {
   };
   
   this.updatepost = {
-    path: '/update/:id',
+    path: '/update',
     methods: ['post'],
     handler: function (req, res, next) {
       res.send('login');
+      Posts.create({
+        userid:'1',
+        status: 0,
+        x:0,
+        y:0,
+        sitename:'7-11南港分店',
+        addr:'南港展覽館',
+        notice:'none',
+        point:0
+      }).success(functioin(final){
+        console.log(final);
+        res.send({
+          res:true
+          postid:1
+        })
+      })
     }
   };
   
